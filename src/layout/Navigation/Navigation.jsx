0d45b5a38ff693fd82d731d0bb992d6aeff6b357
Adam Ciesielski <ciesielskiadam6@gmail.com>
2021-07-16 15:13:52 +0200

    create basic layout

---
+++ src/layout/Navigation/Navigation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Navigation.scss'
+
+const Navigation = () => {
+    return (
+        <nav className='main-navigation'></nav>
+        );
+}
+ 
+export default Navigation;
