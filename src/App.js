7142f1da8ed8003d55c21839c244bd14cfe1ec57
Adam Ciesielski <ciesielskiadam6@gmail.com>
2021-07-18 08:36:00 +0200

    style header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React from 'react';
+
+import Header from './layout/Header/Header';
+import Main from './layout/Main/Main';
+import Footer from './layout/Footer/Footer';
+
 import './App.scss';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Navigation from '../Navigation/Navigation';
 
 import './Header.scss';
 
 const Header = () => {
-  return <header className="header"></header>;
+  return (
+    <header className="header">
+      <p className="header__title">To-do-application</p>
+      <Navigation />
+    </header>
+  );
 };
 
 export default Header;
