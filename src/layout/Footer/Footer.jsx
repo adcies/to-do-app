e9985063fb809ae0e4d2cc4c25b5db7b88f1837b
Adam Ciesielski <ciesielskiadam6@gmail.com>
2021-07-19 09:17:49 +0200

    style footer component

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 import './Footer.scss';
 
 const Footer = () => {
-  return <footer className="footer"></footer>;
+  return (
+    <footer className="footer">
+      <p className="footer__author-info">Created by Adam Ciesielski</p>
+    </footer>
+  );
 };
 
 export default Footer;
