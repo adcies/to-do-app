346c4e4df028e86c2ab2d93082559762cac6da9a
Adam Ciesielski <ciesielskiadam6@gmail.com>
2021-07-26 10:23:55 +0200

    create initial redux boilerplate

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+export const add = () => {
+  return {
+    type: 'ADD',
+  };
+};
+
+export const remove = () => {
+  return {
+    type: 'DELETE',
+  };
+};
+
+export const edit = () => {
+  return {
+    type: 'EDIT',
+  };
+};
---
+++ src/reducers/taskManager.js
@@ -0,0 +1,14 @@
+const taskReducer = (state = '', action) => {
+  switch (action.type) {
+    case 'ADD':
+      return state;
+    case 'DELETE':
+      return state;
+    case 'EDIT':
+      return state;
+    default:
+      return state;
+  }
+};
+
+export default taskReducer;
