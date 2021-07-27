118027a618fdd9dc0df244cd377d49c4a78a58fc
Adam Ciesielski <ciesielskiadam6@gmail.com>
2021-07-27 09:48:43 +0200

    add priorities filter

--- src/components/ToDoList/ToDoList.jsx
+++ src/components/ToDoList/ToDoList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 
 import Task from '../Task/Task';
 
@@ -7,16 +8,42 @@ import { useSelector } from 'react-redux';
 import './ToDoList.scss';
 
 const ToDoList = () => {
+  const [showProrities, setShowProrities] = useState(false);
+
+  const createTasksArray = () => {
+    if (showProrities) {
+      return reversedTasks.filter((task) => task.priority).slice(0, 5);
+    }
+    return reversedTasks.slice(0, 5);
+  };
+
   const tasks = useSelector((state) => state.tasks);
-  const fiveNewestTasks = [...tasks].reverse().slice(0, 5);
+  const reversedTasks = [...tasks].reverse();
+
+  const fiveNewestTasks = createTasksArray();
+
   const tasksElements = fiveNewestTasks.map((task) => (
     <Task key={task.id} taskData={task} />
   ));
 
+  const handleOnChange = () => {
+    setShowProrities((prevValue) => !prevValue);
+  };
+
   return (
     <section className="todo-container">
       <p className="todo-container__info">
-        5 newest todos will be displayed below
+        5 newest todos will be below
+        <label className="todo-container__prorities-label" htmlFor="prorities">
+          Show prorities only
+          <input
+            className="todo-container__prorities-input"
+            id="prorities"
+            type="checkbox"
+            checked={showProrities}
+            onChange={handleOnChange}
+          />
+        </label>
       </p>
       <div className="todo-container__list-wrapper">{tasksElements}</div>
     </section>
