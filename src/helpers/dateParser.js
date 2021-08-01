72b3b331484bf39b5422996dab4248ed119d9b35
Adam Ciesielski <ciesielskiadam6@gmail.com>
2021-08-01 09:32:24 +0200

    fix problem with day dates < 10

--- src/helpers/dateParser.js
+++ src/helpers/dateParser.js
@@ -1,5 +1,9 @@
 export const parseDateToInputFormat = (date) => {
   //parses from locale date string
-  const parsedDate = date.split('.').reverse().join('-');
+  const splitedDate = date.split('.');
+  if (splitedDate[0].length === 1) {
+    splitedDate[0] = '0' + splitedDate[0];
+  }
+  const parsedDate = splitedDate.reverse().join('-');
   return parsedDate;
 };
